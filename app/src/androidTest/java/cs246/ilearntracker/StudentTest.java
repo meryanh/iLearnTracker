9667d9fa2d242eaeedcaf316b00c96da95b7e846
Braden <ste11073@byui.edu>
2015-02-25 09:37:36 -0700

    First test of the Student class with the second function added in

--- app/src/androidTest/java/cs246/ilearntracker/StudentTest.java
+++ app/src/androidTest/java/cs246/ilearntracker/StudentTest.java
@@ -18,5 +18,15 @@ public class StudentTest implements Test {
         Student me = new Student();
         me.setNotify(false);
         assert me.getNotify() == false;
+
+        testInterval(me);
+    }
+
+    public void testInterval(Student me) {
+        me.setCleanUp(604800);
+        assert me.getCleanUpInterval() == 604800;
+
+        me.setRefresh(604800);
+        assert me.getRefreshInterval() == 86400;
     }
 }
