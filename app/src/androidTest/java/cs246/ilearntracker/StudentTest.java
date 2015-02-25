4f73def0eaa4d32d4db2d88f18854a03c71d6037
Braden <ste11073@byui.edu>
2015-02-25 09:34:20 -0700

    First test of the Student class

---
+++ app/src/androidTest/java/cs246/ilearntracker/StudentTest.java
@@ -0,0 +1,22 @@
+package cs246.ilearntracker;
+
+import junit.framework.Test;
+import junit.framework.TestResult;
+
+/**
+ * Created by Braden on 2/25/2015.
+ */
+public class StudentTest implements Test {
+
+    @Override
+    public int countTestCases() {
+        return 0;
+    }
+
+    @Override
+    public void run(TestResult testResult) {
+        Student me = new Student();
+        me.setNotify(false);
+        assert me.getNotify() == false;
+    }
+}
