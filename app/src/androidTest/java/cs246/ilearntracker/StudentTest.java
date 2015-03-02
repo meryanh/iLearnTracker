eece09bf6e7638c5cd9feb74f9be57f80343ff3f
Jared <bea12052@byui.edu>
2015-03-02 08:10:22 -0700

    Updated Student Test Class

--- app/src/androidTest/java/cs246/ilearntracker/StudentTest.java
+++ app/src/androidTest/java/cs246/ilearntracker/StudentTest.java
@@ -1,42 +1,37 @@
 package cs246.ilearntracker;
 
+import android.test.InstrumentationTestCase;
+
 import junit.framework.Test;
 import junit.framework.TestResult;
 
 /**
  * Created by Braden on 2/25/2015.
  */
-public class StudentTest implements Test {
+public class StudentTest extends InstrumentationTestCase {
 
-    @Override
-    public int countTestCases() {
-        return 0;
-    }
-
-    @Override
-    public void run(TestResult testResult) {
+    public void test() throws Exception {
         Student me = new Student();
         me.setNotify(false);
-        assert me.getNotify() == false;
+        assertEquals(me.getNotify(), false);
 
         testInterval(me);
+        testSaveNLoadSettings(me);
     }
 
     public void testInterval(Student me) {
         me.setCleanUp(604800);
-        assert me.getCleanUpInterval() == 604800;
+        assertEquals(me.getCleanUpInterval(), 604800);
 
         me.setRefresh(604800);
-        assert me.getRefreshInterval() == 86400;
+        assertEquals(me.getRefreshInterval(), 86400);
     }
 
     public void testSaveNLoadSettings(Student me) {
         me.saveSettings();
         me.loadSettings();
-        assert me.getCleanUpInterval() == 604800;
-        assert me.getRefreshInterval() == 604800;
-        assert me.getNotify() == false;
-
-
+        assertEquals(me.getCleanUpInterval(), 604800);
+        assertEquals(me.getRefreshInterval(), 604800);
+        assertEquals(me.getNotify(), false);
     }
 }
