01f099265ad4f6b8f7958b728ace764132bf174d
Jared <bea12052@byui.edu>
2015-03-06 09:25:44 -0700

    Add Class works

--- app/src/main/java/cs246/ilearntracker/AddClass.java
+++ app/src/main/java/cs246/ilearntracker/AddClass.java
@@ -12,18 +12,10 @@ public class AddClass extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //System.out.println("Here!\n");
+
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(iLearnTracker.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(20);
-        textView.setText("Your favorite scripture is:\n" + message);
-        setContentView(textView);
-/*
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(iLearnTracker.EXTRA_MESSAGE);
-        setContentView(R.layout.activity_add_class);*/
+        setContentView(R.layout.activity_add_class);
     }
 
     @Override
