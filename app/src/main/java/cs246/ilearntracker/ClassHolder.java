034a620ebf0629f6172e009b17f6152de03e2a28
Ryan Hildreth <meryanh@gmail.com>
2015-03-10 17:01:03 -0600

    More work on the class_list_layout.xml and classListAdapter.java files.

---
+++ app/src/main/java/cs246/ilearntracker/ClassHolder.java
@@ -0,0 +1,11 @@
+package cs246.ilearntracker;
+
+import android.widget.TextView;
+
+import java.lang.*;
+
+public class ClassHolder {
+    Class aClass;
+    TextView classButton;
+    TextView color;
+}
